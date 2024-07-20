77c48cf02c34180a87e233ca9204c282ab9134c2
Crejohn <142834355+Crejohn@users.noreply.github.com>
2024-07-19 21:24:03 -0400

    website update

--- assets/js/main.js
+++ assets/js/main.js
@@ -197,7 +197,70 @@
     selector: '.glightbox'
   });
 
- 
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Initiate portfolio details lightbox 
+   */
+  const portfolioDetailsLightbox = GLightbox({
+    selector: '.portfolio-details-lightbox',
+    width: '90%',
+    height: '90vh'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+
   /**
    * Animation on scroll function and init
    */
@@ -214,29 +277,21 @@
   });
 
 })()
-
 /**
- * Gallery RRSS
+ * Video sound on/off when clicking on video
  */
-  $.noConflict();
-jQuery(document).ready(function($) {
-       $('.slider').slick({
-      centerMode: true,
-      dots: true,
-      autoplay: true,
-      centerPadding: '60px',
-      slidesToShow: 3,
-      responsive: [{
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          Infinity: true,
-          slidesToShow: 1
-        },
-      }]
-    });
-});
+$(document).ready(function(){
+  $("video").prop('muted', true);
 
+  $("video").click( function (){
+      $(this).prop('muted', !$(this).prop('muted'));
+  });
+}); 
+/**
+ * Video sound off when clicking on prev/next buttons
+ */
+const myCarousel = document.getElementById('video_wrapper')
 
+myCarousel.addEventListener('slide.bs.carousel', event => {
+  $("video").prop('muted', true);
+})
